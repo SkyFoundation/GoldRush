8f61f507a91b67179d0345f69281f82dbb3836fc
WisdomJack <Wisdomjack143@outlook.com>
2020-03-29 17:09:22 +0800

    fixed biome registery

--- src/main/java/com/pixelsky/cheese/init/CheeseBiomes.java
+++ src/main/java/com/pixelsky/cheese/init/CheeseBiomes.java
@@ -3,14 +3,21 @@ package com.pixelsky.cheese.init;
 import com.pixelsky.cheese.Reference;
 import com.pixelsky.cheese.world.biome.BiomeCheese;
 import com.pixelsky.cheese.world.biome.BiomeCheeseDimension;
+import net.minecraft.block.Block;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.BiomeProperties;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.common.BiomeManager.BiomeEntry;
 import net.minecraftforge.common.BiomeManager.BiomeType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+
+@Mod.EventBusSubscriber(modid = "dgm")
 public class CheeseBiomes {
 	
 	public static Biome CHEESE_BIOME;
@@ -18,18 +25,18 @@ public class CheeseBiomes {
 	
 	public CheeseBiomes() {
 		init();
-		register();
 	}
 	
 	public static void init() {
 		CHEESE_BIOME = new BiomeCheese(new BiomeProperties(TextFormatting.YELLOW + "Cheese" + TextFormatting.RESET).setWaterColor(0xD6FF42).setRainDisabled()).setRegistryName(Reference.MODID, "cheese_biome");
 		CHEESE_DIMENSION_BIOME = new BiomeCheeseDimension(new BiomeProperties(TextFormatting.YELLOW + "Cheese Dimension" + TextFormatting.RESET).setWaterColor(0xD6FF42).setRainDisabled()).setRegistryName(Reference.MODID, "cheese_dimension_biome");
 	}
-	
-	public static void register() {
+
+	@SubscribeEvent
+	public static void register(RegistryEvent.Register<Biome> event)  {
 		BiomeManager.addBiome(BiomeType.WARM, new BiomeEntry(CHEESE_BIOME, 3));
-		GameRegistry.register(CHEESE_BIOME);
-		GameRegistry.register(CHEESE_DIMENSION_BIOME);
+		event.getRegistry().register(CHEESE_BIOME);
+		event.getRegistry().register(CHEESE_DIMENSION_BIOME);
 	}
 	
 }
