7d67d4d5a612b0b2d5fe77000667a8141545471f
Beanflame <2962672241@qq.com>
2020-04-12 19:32:37 +0800

    + 芯片

--- src/main/java/com/pixelsky/goldrush/init/ModItems.java
+++ src/main/java/com/pixelsky/goldrush/init/ModItems.java
@@ -1,17 +1,23 @@
 package com.pixelsky.goldrush.init;
 
 
+import com.pixelsky.goldrush.items.materials.ItemChip;
 import com.pixelsky.goldrush.items.robots.ItemRobotDestroyer;
 import net.minecraft.item.Item;
 
 public class ModItems implements IRegistery {
+	
 public static final ItemRobotDestroyer ITEM_BLOCK_DESTROYER=new ItemRobotDestroyer();
+
+public static final Item ITEM_chip = new ItemChip();
+
 	
 	public ModItems() {
 		register();
 	}
 	public void register() {
-	regIm(ITEM_BLOCK_DESTROYER);
+		regIm(ITEM_BLOCK_DESTROYER);
+		regIm(ITEM_chip);
 	}
 	private void regIm(Item item){
 		RegistrationHandler.Items.add(item);
--- src/main/java/com/pixelsky/goldrush/items/materials/ItemChip.java
+++ src/main/java/com/pixelsky/goldrush/items/materials/ItemChip.java
@@ -1,4 +1,32 @@
 package com.pixelsky.goldrush.items.materials;
+
+import com.pixelsky.goldrush.Reference;
+import com.pixelsky.goldrush.init.CreativeTabs;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+
 //芯片
-public class ItemChip {
+public class ItemChip extends Item
+{
+	private final String name="chip";
+	
+	public ItemChip()
+	{
+		super();
+		setRegistryName(new ResourceLocation(Reference.MODID, name))
+		.setTranslationKey(name)
+		.setCreativeTab(CreativeTabs.GOLD_RUSH);
+	        /*
+	
+        setCreativeTab(UCECreativeTab.UCETAB);
+        
+        setTranslationKey("uce.bucket_wheel");
+        
+        setRegistryName(UCEmod.MODID, "bucket_wheel");
+        */
+    }
+    
+	
+	
 }
