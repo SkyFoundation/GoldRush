aa7593b0be8cab954889f169c55fd33b329f630e
Beanflame <2962672241@qq.com>
2020-04-17 11:34:11 +0800

    修复

--- src/main/java/com/pixelsky/goldrush/Reference.java
+++ src/main/java/com/pixelsky/goldrush/Reference.java
@@ -3,9 +3,10 @@ package com.pixelsky.goldrush;
 public class Reference {
 	
 	public static final String MODID = "goldrush";
-	public static final String NAME = "淘金热";
-	public static final String VERSION = "1.11.2.0";
+	public static final String NAME = "Gold Rush";
+	public static final String VERSION = "1.2.0";
 	public static final String CLIENTPROXY = "com.pixelsky.goldrush.proxy.ClientProxy";
 	public static final String COMMONPROXY = "com.pixelsky.goldrush.proxy.CommonProxy";
 	
 }
+//Gold Rush
