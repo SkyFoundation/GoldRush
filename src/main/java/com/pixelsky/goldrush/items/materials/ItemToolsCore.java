29cbaba12a7bf81a1a4deb0e09b090d253f0c2f4
Beanflame <2962672241@qq.com>
2020-04-18 22:04:43 +0800

    +++ 工具更新

---
+++ src/main/java/com/pixelsky/goldrush/items/materials/ItemToolsCore.java
@@ -0,0 +1,22 @@
+package com.pixelsky.goldrush.items.materials;
+
+import com.pixelsky.goldrush.Reference;
+import com.pixelsky.goldrush.init.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+
+
+//工具核心
+public class ItemToolsCore extends Item
+{
+	private final String name="tools_core";
+	
+	public ItemToolsCore()
+	{
+		super();
+		setRegistryName(new ResourceLocation(Reference.MODID, name))
+		.setTranslationKey(name)
+		.setCreativeTab(CreativeTabs.GOLD_RUSH);
+    }
+
+}
